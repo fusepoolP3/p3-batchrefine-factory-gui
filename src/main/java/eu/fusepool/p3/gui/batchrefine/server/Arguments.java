b75c223b3c84c51c6ae9d9d0837bbb9bfdc9579a
Pascal Mainini <pascal@mainini.ch>
2015-04-16 19:43:10 +0200

    changed default port from 8101 to 8203

--- src/main/java/eu/fusepool/p3/gui/batchrefine/server/Arguments.java
+++ src/main/java/eu/fusepool/p3/gui/batchrefine/server/Arguments.java
@@ -6,7 +6,7 @@ import org.wymiwyg.commons.util.arguments.CommandLine;
 public interface Arguments extends ArgumentsWithHelp {
     
     @CommandLine(longName = "port", shortName = {"P"}, required = false,
-            defaultValue = "8101",
+            defaultValue = "8203",
             description = "The port on which the batchrefine tranformer factory shall listen")
     public int getPort();
     
